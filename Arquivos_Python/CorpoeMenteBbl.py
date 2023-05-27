a89a381f6e511c3aa7e6c3747ab7e3eaa051fe4c
Luciana Otávio Nunes <129115014+luhonunes@users.noreply.github.com>
2023-05-27 14:50:43 -0300

    Adiciono Arquivo Bbl para funções fora da Classe

---
+++ Arquivos_Python/CorpoeMenteBbl.py
@@ -0,0 +1,9 @@
+
+def linha():   # Função apenas para separação estética dos blocos 
+    print('-'*160)
+    return
+
+def linha2():  # Função apenas para separação estética dos blocos 
+    print('='*160)
+    return
+
